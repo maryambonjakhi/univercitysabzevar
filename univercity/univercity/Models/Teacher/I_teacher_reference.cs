3562d3eb1dd540690d425bca3199da648e1002b6
masoomeshamsabadi <fmezadi9304265430@gmail.com>
2019-06-23 00:33:21 +0430

    add repository reference add tbl_teacher_reference

---
+++ univercity/univercity/Models/Teacher/I_teacher_reference.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using univercity.Models;
+
+namespace univercity.Models
+{
+ public   interface I_teacher_reference
+    {
+
+        int insert_teacher_reference(tbl_teacher_reference tb);
+        int delete_teacher_reference(tbl_teacher_reference tb);
+        int update_teacher_reference(tbl_teacher_reference tb);
+        
+       tbl_teacher_reference find_teacher_reference(string code_national_teacher);
+        List<tbl_teacher_reference> find_teachers_reference(string code_national_teacher);
+    }
+}
---
+++ univercity/univercity/Models/Teacher/Teacher_reference_Repository.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace univercity.Models.Teacher
+{
+    public class Teacher_reference_Repository : I_teacher_reference
+    {
+        univercityEntities db = new univercityEntities();
+
+        public int delete_teacher_reference(tbl_teacher_reference tb)
+        {
+            db.tbl_teacher_reference.Remove(tb);
+            return db.SaveChanges();
+        }
+
+        public List<tbl_teacher_reference> find_teachers_reference(string code_national_teacher)
+        {
+            throw new NotImplementedException();
+        }
+
+        public tbl_teacher_reference find_teacher_reference(string code_national_teacher)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int insert_teacher_reference(tbl_teacher_reference tb)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int update_teacher_reference(tbl_teacher_reference tb)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
